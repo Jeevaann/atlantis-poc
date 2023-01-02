ce007b0e52d9de9375a91675fd4e405a04e3f963
Jeevan Darapu <jeevan.darapu@ibm.com>
2023-01-02 14:25:21 +0530

    deleted ec2.tf file

--- modules/web-server/main.tf
+++ modules/web-server/main.tf
@@ -13,8 +13,6 @@
 # Outputs 
 ##########################
 
-resource "null_resource" "example"{}
-
 resource "null_resource" "demo-atlantis"{}
 
 ####################################################################
