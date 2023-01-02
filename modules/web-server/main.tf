c0a0d234563bd82fb52a668a1b8fedbadf77ad6b
Jeevan Darapu <jeevan.darapu@ibm.com>
2023-01-02 14:58:16 +0530

    commented aws_instance block

--- modules/web-server/main.tf
+++ modules/web-server/main.tf
@@ -334,7 +334,7 @@ output "asg_arn" {
   value = aws_autoscaling_group.dev.arn
 }
 
-resource "aws_instance" "webserver" {
-    ami = "ami-0b5eea76982371e91"
-    instance_type = "t2.micro"
-}
+# resource "aws_instance" "webserver" {
+#     ami = "ami-0b5eea76982371e91"
+#     instance_type = "t2.micro"
+# }
